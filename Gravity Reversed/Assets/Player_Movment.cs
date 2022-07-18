af152372f3f170858bc714ea48f3d85118184b2b
Rafe3t <109312927+Rafe3t@users.noreply.github.com>
2022-07-18 23:27:34 +0200

    Working on Player
    Ana el character beyet7arak lama betdoos (a)
    
    2od5ol shoofo , haye3gebak

---
+++ Gravity Reversed/Assets/Player_Movment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movment : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+    }
+}
